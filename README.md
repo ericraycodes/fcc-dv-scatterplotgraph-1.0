196b1d0de52803e254158f8e344a9862840e4a4b
lasernoy <cloudy.nny07@gmail.com>
2024-05-16 21:46:57 +0800

    plot

--- README.md
+++ README.md
@@ -13,3 +13,4 @@ Graph data of top 35 fastest bicycle racing record - in particular, through a sc
 
 ## References
  1. D3 docs: [d3js.org](https://d3js.org/d3-scale/time)
+ 1. D3 Time Series Axis: [https://www.developer.com/design/working-with-d3-time-series-axes/](https://www.developer.com/design/working-with-d3-time-series-axes/)
--- script.js
+++ script.js
@@ -28,7 +28,8 @@ function graphData(data) {
 		.range([padLeft, w - padRight]);
 	// y-scale
 	const parseTime = d3.timeParse("%M:%S");
-	const yScale = d3.scaleTime()
+	console.log('parse time', parseTime("30:04"));
+	const yScale = d3.scaleUtc()
 		.domain([
 			d3.min(data, d => parseTime(d.Time)), 
 			d3.max(data, d => parseTime(d.Time))
@@ -45,18 +46,30 @@ function graphData(data) {
 	const xAxis = d3.axisBottom(xScale)
 		.tickFormat(year => year.toString());
 	svg.append("g")
-		.attr("transform", `translate(0, ${h - padBottom})`)
-		.call(xAxis);
+		.call(xAxis)
+		.attr("id", "x-axis")
+		.attr("transform", `translate(0, ${h - padBottom})`);
+	// y-axis
+	const yAxis = d3.axisLeft(yScale)
+		.ticks(d3.timeSecond.every(15))
+	svg.append("g")
+		.call(yAxis)
+		.attr("id", "y-axis")
+		.attr("transform", `translate(${padLeft}, 0)`)
 
 	// plots
 	const circle = svg.selectAll("circle")
 		.data(data)
 		.enter()
 		.append("circle")
+		.attr("class", "dot")
 		.attr("r", 7)
 		.attr("cx", d => xScale(parseInt(d.Year)))
-		.attr("cy", d => padTop + yScale(parseTime(d.Time)))
-		.style("fill", d => d.Doping==="" ? "green":"red");
+		.attr("cy", d => yScale(parseTime(d.Time)))
+		.attr("data-xvalue", d => parseInt(d.Year))
+		.attr("data-yvalue", d => parseTime(d.Time))
+		.style("fill", d => d.Doping==="" ? "green":"red")
+		.style("opacity", 0.7);
 }
 
 
